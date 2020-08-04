c2c6e1a036e27280d67b76a9cbedb34dd7022070
sojivarkey <sojivarkey@sojis-air>
2020-08-03 19:47:03 -0500

    Initial project Add

---
+++ src/test/java/manj/springframework/springframeworkpetclinic/SpringframeworkPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package manj.springframework.springframeworkpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
