6d5325a49543d74c27cd237b9e42c99262114bfc
manjushasoji <manjushajoy@hotmail.com>
2020-09-05 19:59:52 -0500

    added Spring Data JPA services to Pet, PetType,Speciality and Visit. closes #41 , #42 , #43 , #44

---
+++ petclinic-data/src/main/java/manj/springframework/springframeworkpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package manj.springframework.springframeworkpetclinic.services;
+
+import manj.springframework.springframeworkpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+
+}
---
+++ petclinic-data/src/main/java/manj/springframework/springframeworkpetclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,52 @@
+package manj.springframework.springframeworkpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import manj.springframework.springframeworkpetclinic.model.Speciality;
+import manj.springframework.springframeworkpetclinic.repositories.SpecialityRepository;
+import manj.springframework.springframeworkpetclinic.services.SpecialityService;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+
+	private final SpecialityRepository specialityRepository;
+
+	public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+		this.specialityRepository = specialityRepository;
+	}
+
+	@Override
+	public Set<Speciality> findAll() {
+		Set<Speciality> specialities = new HashSet<>();
+		specialityRepository.findAll().forEach(specialities::add);
+		return specialities;
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return specialityRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		return specialityRepository.save(object);
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		specialityRepository.delete(object);
+
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		specialityRepository.deleteById(id);
+
+	}
+
+}
