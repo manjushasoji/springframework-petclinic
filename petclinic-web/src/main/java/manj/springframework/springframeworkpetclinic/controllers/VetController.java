e7202b485103b3110b9c1349661ba19949a12abc
manjushasoji <manjushajoy@hotmail.com>
2020-11-14 18:00:20 -0600

    added JSON endpoint for Vets. closes #68.

--- petclinic-web/src/main/java/manj/springframework/springframeworkpetclinic/controllers/VetController.java
+++ petclinic-web/src/main/java/manj/springframework/springframeworkpetclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package manj.springframework.springframeworkpetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import manj.springframework.springframeworkpetclinic.model.Vet;
 import manj.springframework.springframeworkpetclinic.services.VetService;
 
 @Controller
@@ -19,6 +23,11 @@ public class VetController {
 	public String listVets(Model model) {
 		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
+	}
+
+	@RequestMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetsJson() {
+		return vetService.findAll();
 	}
 
 }
